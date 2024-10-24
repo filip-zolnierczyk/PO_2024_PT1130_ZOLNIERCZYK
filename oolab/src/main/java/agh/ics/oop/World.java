f356cd31e96154d882c6e343fb6ac3c5f6d4fd82
filip-zolnierczyk <filip.zolnierczyk@gmail.com>
2024-10-24 16:51:18 +0200

    naprawione zadanie

---
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,33 @@
+package agh.ics.oop;
+
+import agh.ics.oop.model.MoveDirection;
+
+public class World {
+    public static void main(String[] args) {
+        System.out.println("system wystartował");
+
+        MoveDirection[] directions = OptionsParser.parse(args);
+        run(directions);
+
+        System.out.println("system zakończył działanie");
+    }
+
+    public static void run(MoveDirection[] directions) {
+        for (MoveDirection direction : directions) {
+            switch (direction) {
+                case FORWARD:
+                    System.out.println("Zwierzak idzie do przodu");
+                    break;
+                case BACKWARD:
+                    System.out.println("Zwierzak idzie do tyłu");
+                    break;
+                case RIGHT:
+                    System.out.println("Zwierzak skręca w prawo");
+                    break;
+                case LEFT:
+                    System.out.println("Zwierzak skręca w lewo");
+                    break;
+            }
+        }
+    }
+}
